4c6327ad6a22e45491d2c4efbeda3633d94ef419
zero24x <zero@you-spam.com>
2018-08-16 00:30:17 +0200

    new seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -114,10 +114,8 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        vSeeds.push_back(CDNSSeedData("0","207.246.111.251"));
-
-
-
+        vSeeds.push_back(CDNSSeedData("node.bugo.club", "node.bugo.club"));
+        vSeeds.push_back(CDNSSeedData("node.bugo.space", "node.bugo.space"));
 
         convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
